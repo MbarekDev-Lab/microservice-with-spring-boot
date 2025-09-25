7a403dea98f1b7e647e1e17110420da8c5626cd2
Mbarek <mbarek.raiss@gmail.com>
2025-09-25 09:04:22 +0200

    Initial setup of microservices projects

---
+++ microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','MAD',10,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','MAD',20,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','MAD',30,'');
